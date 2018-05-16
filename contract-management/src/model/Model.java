20fcfa20a24e613a72702a4e5d7e37ef5e88de2e
Alex0702 <39345675+Alex0702@users.noreply.github.com>
2018-05-16 20:29:25 +0200

    Testerei

--- contract-management/src/model/Model.java
+++ contract-management/src/model/Model.java
@@ -1,7 +1,8 @@
 package model;
 
 public class Model {
-	private String title = "Vertragsverwaltung";
+	private String title = "Auch hier teste ich nur";
+	private int test = 11000;
 	
 	public String getTitle(){
 		return title;
