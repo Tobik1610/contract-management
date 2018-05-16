61886f26c954012b53c20ee0d7264e2e17aff52a
Tobik1610 <39301495+Tobik1610@users.noreply.github.com>
2018-05-16 20:37:57 +0200

    Test

--- contract-management/src/model/Model.java
+++ contract-management/src/model/Model.java
@@ -2,7 +2,7 @@ package model;
 
 public class Model {
 	private String title = "Auch hier teste ich nur";
-	private int test = 11000;
+	private int test = 1100011;
 	
 	public String getTitle(){
 		return title;
